7f4c39eab1ababe3cf227b4366c3f0a9c157645f
Xavier Garcia Buils <xgbuils@gmail.com>
2021-01-05 15:06:42 +0100

    more changes

--- main.js
+++ main.js
@@ -10,16 +10,127 @@ const h = (tagName, children) => {
   return el;
 };
 
-const app = document.getElementById('app');
+const SimpleFormatter = formatter => ({
+  format(amount) {
+    return [
+      {
+        value: formatter.format(amount),
+        type: 'integer',
+      },
+    ];
+  },
+});
 
-app.innerHTML = '';
+const MoneyTokenizer = parts => {
+  const regexp = /([ig]+l?d?l?)|(l?cl?)|(f)/g;
+  const tokenTypes = ['integer', 'currency', 'fraction'];
+  const codifiedParts = parts.map(part => part.type.substring(0, 1)).join('');
+  const transformToken = (position, codifiedPart) => {
+    return codifiedPart
+      .split('')
+      .map((_, index) => {
+        return parts[position + index].value;
+      })
+      .join('');
+  };
+  const getToken = matches => {
+    const tokenIndex = matches.slice(1).findIndex(match => match);
+    return {
+      value: transformToken(matches.index, matches[tokenIndex + 1]),
+      type: tokenTypes[tokenIndex],
+    };
+  };
+  return {
+    getToken() {
+      const matches = regexp.exec(codifiedParts);
+      return matches && getToken(matches);
+    },
+  };
+};
 
-const content = amount => {
+const PartsFormatter = formatter => ({
+  format(amount) {
+    const parts = formatter.formatToParts(amount);
+    const tokenizer = MoneyTokenizer(parts);
+    const moneyParts = [];
+    let token = tokenizer.getToken();
+    while (token) {
+      moneyParts.push(token);
+      token = tokenizer.getToken();
+    }
+    return moneyParts;
+  },
+});
+
+const SpecialNumberLocaleFormatter = (
+  currencyLocaleFormatter,
+  numberLocaleFormatter,
+  styled
+) => ({
+  format(amount) {
+    const numberLocaleParts = new Map(
+      numberLocaleFormatter.format(amount).map(part => [part.type, part])
+    );
+    const currencyLocaleParts = currencyLocaleFormatter.format(amount);
+    const moneyParts = currencyLocaleParts.map(part =>
+      part.type !== 'currency' ? numberLocaleParts.get(part.type) : part
+    );
+    if (styled) {
+      return moneyParts;
+    }
+    return [
+      {
+        type: 'default',
+        value: toMoneyText(moneyParts),
+      },
+    ];
+  },
+});
+
+const browserDoesNotSupportFormatToParts = formatter =>
+  typeof formatter.formatToParts !== 'function';
+
+const createFormatter = (currency, locale, minimumFractionDigits) =>
+  new Intl.NumberFormat(locale, {
+    style: 'currency',
+    currency,
+    minimumFractionDigits,
+  });
+
+const MoneyFormatter = (
+  { locale, numberLocale, minimumFractionDigits },
+  currency,
+  styled
+) => {
+  const formatter = createFormatter(currency, locale, minimumFractionDigits);
+  if (
+    (!styled && !numberLocale) ||
+    browserDoesNotSupportFormatToParts(formatter)
+  ) {
+    return SimpleFormatter(formatter);
+  }
+  const partsFormatter = PartsFormatter(formatter);
+  if (!numberLocale) {
+    return partsFormatter;
+  }
+  return SpecialNumberLocaleFormatter(
+    partsFormatter,
+    PartsFormatter(
+      createFormatter(currency, numberLocale, minimumFractionDigits)
+    ),
+    styled
+  );
+};
+
+const test = amount => {
   try {
-    return JSON.stringify(Intl.NumberFormat('en-UK', {
-      style: 'currency',
-      currency: 'USD',
-    }).formatToParts(amount));
+    const locale = {
+      locale: 'en-UK'
+    };
+    const currency = 'USD';
+    const formatter = MoneyFormatter(locale, currency, true);
+    const moneyParts = formatter.format(amount);
+    return JSON.stringify(moneyParts);
   } catch (error) {
     return error.message;
   }
@@ -27,9 +138,11 @@ const content = amount => {
 
 const Money = (amount) => h('div', [
   h('span', `${amount}: `),
-  h('span', content(amount))
+  h('span', test(amount))
 ]);
 
+const app = document.getElementById('app');
+app.innerHTML = '';
 document.getElementById('app').appendChild(
   h(
     'div', [
